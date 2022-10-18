37bdad4f1019d36f33e2802207dcfd60ebd7e47e
Ken Power <github@kenpower.com>
2022-10-18 22:47:21 +0100

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,22 +1,22 @@
 #include "pch.h"
 #include "TicTacToe.h"
 
-TEST(TicTacToe, FirstAvailableMove5) {
+TEST(TicTacToe, ValidMove5) {
 	TicTacToe ttt("XOXOX-OXO");
 	EXPECT_EQ(5, ttt.move('X'));
 }
 
-TEST(TicTacToe, FirstAvailableMove8) {
+TEST(TicTacToe, ValidMove8) {
 	TicTacToe ttt("XOXOXOOX-");
 	EXPECT_EQ(8, ttt.move('X'));
 }
 
-TEST(TicTacToe, FirstAvailableMove0) {
+TEST(TicTacToe, ValidMove0) {
 	TicTacToe ttt("---------");
 	EXPECT_EQ(0, ttt.move('X'));
 }
 
-TEST(TicTacToe, FirstAvailableMoveNone) {
+TEST(TicTacToe, ValidMoveNone) {
 	TicTacToe ttt("XXXXXXXX");
 	EXPECT_EQ(-1, ttt.move('X'));
 }
